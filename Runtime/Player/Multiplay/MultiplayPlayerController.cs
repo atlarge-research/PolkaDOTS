7f2772b951a68e8388981c81fffca2badd380562
ZainMunir <zainmunir999@gmail.com>
2024-06-25 11:22:19 +0200

    change: removed power - now inputs

--- Runtime/Player/Multiplay/MultiplayPlayerController.cs
+++ Runtime/Player/Multiplay/MultiplayPlayerController.cs
@@ -29,7 +29,7 @@ namespace PolkaDOTS.Multiplay
         [DontSerialize] public bool playerEntityExists;
         [DontSerialize] public bool playerEntityRequestSent;
         [DontSerialize] public Entity playerEntity;
-        [DontSerialize] public List<int> selectableItems = new List<int> { 16, 17, 19, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        [DontSerialize] public List<int> selectableItems = new List<int> { 1, 15, 17, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
 
         protected void Awake()
         {
